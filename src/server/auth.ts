899536df42413034f7d5c938aedf460f76d018ca
Milo CARTAL <105420069+milocartal@users.noreply.github.com>
2023-05-26 13:50:16 +0200

    update prisma

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -5,7 +5,6 @@ import {
   type NextAuthOptions,
   type DefaultSession,
 } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
 import GithubProvider from "next-auth/providers/github"
 import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
