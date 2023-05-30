15ddefa27337eadc88e341fc41ae2d486b2b312c
Hélène SCHOONHEERE <helene.reva.s@protonmail.com>
2023-05-30 14:20:42 +0200

    fixes notifs and promo modal

--- src/pages/components/notifs.tsx
+++ src/pages/components/notifs.tsx
@@ -4,45 +4,54 @@ import { useState } from "react";
 
 function Notifs () {
     const [notifs, setNotifs] = useState(true)
-    const [open, setOpen] = useState(true)
+    const [open, setOpen] = useState(false)
+
+    const DATA = [
+        {
+            "id": 1,
+            "nom": "Lorem Ipsum",
+            "message": "A rendu le devoir lorem ipsum dolor sit amet"
+
+        },
+        {
+            "id": 2,
+            "nom": "Lorem Ipsum",
+            "message": "Vous a envoyé un message"
+        },
+        {
+            "id": 3,
+            "nom": "Lorem Ipsum",
+            "message": "A rendu le devoir lorem ipsum dolor sit amet"
+
+        },
+        {
+            "id": 4,
+            "nom": "Lorem Ipsum",
+            "message": "Vous a envoyé un message"
+        }
+    ]
 
     return(
         <div className="fixed bottom-16 right-16 w-[300px] h-[400px]">
             {open && 
-                <div className="bg-white w-full h-[300px] flex flex-col items-start p-5 overflow-hidden rounded-lg">
+                <div className="bg-white w-full h-[300px] flex flex-col items-start p-5 overflow-auto rounded-lg">
                     <button className="self-end">
                         <p className="text-sm mr-2">Tout effacer</p>
                     </button>
-                    <span className="flex flex-row justify-start items-center w-full mt-5">
-                        <img src="userPFP.png" className="w-12 h-12 rounded-full object-cover mr-3" alt="Photo de profil utilisateur"/>
-                        <div>
-                            <p className="text-base text-black font-semibold">Lorem ipsum</p>
-                            <p className="text-sm text-black mr-1">A rendu le devoir lorem ipsum dolor sit amet</p>
-                        </div>
-                        <button className="w-[10%]">
-                            <BiX className="text-3xl text-[#2EA3A5]"/>
-                        </button>
-                    </span>
-                    <span className="flex flex-row justify-start items-center w-full mt-5">
-                        <img src="userPFP.png" className="w-12 h-12 rounded-full object-cover mr-3" alt="Photo de profil utilisateur"/>
-                        <div>
-                            <p className="text-base text-black font-semibold">Lorem ipsum</p>
-                            <p className="text-sm text-black mr-1">Vous a envoyé un message</p>
-                        </div>
-                        <button className="w-[10%]">
-                            <BiX className="text-3xl text-[#2EA3A5]"/>
-                        </button>
-                    </span>
-                    <span className="flex flex-row justify-start items-center w-full mt-5">
-                        <img src="userPFP.png" className="w-12 h-12 rounded-full object-cover mr-3" alt="Photo de profil utilisateur"/>
-                        <div>
-                            <p className="text-base text-black font-semibold">Lorem ipsum</p>
-                            <p className="text-sm text-black mr-1">A rendu le devoir lorem ipsum dolor sit amet</p>
-                        </div>
-                        <button className="w-[10%]">
-                            <BiX className="text-3xl text-[#2EA3A5]"/>
-                        </button>
-                    </span>
+                    {DATA.map((item) => {
+                        return (
+                            <span className="flex flex-row justify-start items-center w-full mt-5" key={item.id}>
+                                <img src="userPFP.png" className="w-12 h-12 rounded-full object-cover mr-3" alt="Photo de profil utilisateur"/>
+                                <div>
+                                    <p className="text-base text-black font-semibold">{item.nom}</p>
+                                    <p className="text-sm text-black mr-1">{item.message}</p>
+                                </div>
+                                <button className="w-[10%]">
+                                    <BiX className="text-3xl"/>
+                                </button>
+                            </span>
+                        )
+                    })}
                 </div>
             }
             <button className="fixed bottom-16 right-16 w-20 h-20 bg-[#2EA3A5] flex flex-row items-center justify-center rounded-full" onClick={() => setOpen(!open)}>
--- src/pages/components/promo.tsx
+++ src/pages/components/promo.tsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import { BiChevronDown } from "react-icons/bi";
 
 function Promo () {
+  const [open, setOpen] = useState(false)
+
     return(
-        <button className="flex flex-row items-center justify-between px-5 py-2 bg-[#0E6073] text-white rounded-lg">
+      <div className="relative">
+        <button className="flex flex-row items-center justify-between px-5 py-2 bg-[#0E6073] text-white rounded-lg" onClick={() => setOpen(!open)}>
           <p className="text-base mr-2">Promo 1 2022/2023</p>
           <BiChevronDown className="text-4xl" />
         </button>
+        {open && 
+        <div className="w-full absolute bg-white rounded-b-lg flex flex-col items-center divide-y divide-[#0E6073]">
+          <p className="text-sm text-[#0E6073] py-4">Promo 2 2022/2023</p>
+          <p className="text-sm text-[#0E6073] py-4">Promo 3 2022/2023</p>
+          <p className="text-sm text-[#0E6073] py-4">Promo 4 2022/2023</p>
+          <div className="flex flex-row items-center justify-center bg-[#0E6073] text-white rounded-b-lg w-full h-12">
+            <p className="text-sm">+ Créer une promo</p>
+          </div>
+        </div>
+        }
+      </div>
 )}
 
 export default Promo;
