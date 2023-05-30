004bdbea505d7362c0dbba748dcd0857da4ea118
Milo CARTAL <105420069+milocartal@users.noreply.github.com>
2023-05-30 16:39:59 +0200

    fix 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -8,7 +8,7 @@ export default function Custom404() {
       </Head>
       <main className="flex flex-col h-screen w-full items-center justify-center gap-10">
         <img src="https://media.discordapp.net/attachments/688793736620146689/915869475423813662/20210709_215217.gif" alt="Les erreurs 404, envie de crever" className="max-w-[50%]" />
-        <h1>Et merde, une 404, j'en ai marre</h1>
+        <h1>Et merde, une 404, j&apos;en ai marre</h1>
       </main>
     </>);
 }
