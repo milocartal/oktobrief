b981ee676c27c11cd2bb10f7b2efa09361e3aef3
Milo CARTAL <105420069+milocartal@users.noreply.github.com>
2023-05-30 11:22:35 +0200

    dashboard user and super admin

---
+++ src/server/api/routers/promo.ts
@@ -0,0 +1,102 @@
+import { z } from "zod";
+import {
+  createTRPCRouter,
+  publicProcedure,
+  protectedProcedure,
+} from "~/server/api/trpc";
+import { prisma } from "~/server/db";
+
+export const promoRouter = createTRPCRouter({
+
+
+  getAll: protectedProcedure.query(() => {
+    return prisma.promo.findMany({
+      include: {
+        apprenants: true,
+        referentiel: true,
+      }
+    });
+  }),
+
+  getAllForUser: protectedProcedure.input(z.object({ id: z.string() })).query(({ input }) => {
+    return prisma.promo.findMany({
+      where: {
+        apprenants: {
+          some: {
+            id: input.id
+          }
+        }
+      },
+      include: {
+        apprenants: true,
+        referentiel: true,
+      }
+    })
+  }),
+
+  create: protectedProcedure.input(z.object({ title: z.string(), desc: z.string(), idRef: z.string(), start: z.date(), end: z.date() })).mutation(({ input }) => {
+    return prisma.promo.create({
+      data: {
+        title: input.title,
+        description: input.title,
+        idRef: input.idRef,
+        starting: input.start,
+        ending: input.end
+      }
+    })
+  }),
+
+  delete: protectedProcedure.input(z.object({ id: z.string() })).mutation(({ input }) => {
+    return prisma.promo.delete({
+      where: {
+        id: input.id
+      }
+    })
+  }),
+
+  update: protectedProcedure.input(z.object({ id: z.string(), title: z.string(), desc: z.string(), idRef: z.string(), start: z.date(), end: z.date() })).mutation(({ input }) => {
+    return prisma.promo.update({
+      where:{
+        id: input.id
+      },
+      data: {
+        title: input.title,
+        description: input.title,
+        idRef: input.idRef,
+        starting: input.start,
+        ending: input.end
+      }
+    })
+  }),
+
+  addStudent: protectedProcedure.input(z.object({ id: z.string(), idU: z.string() })).mutation(({ input }) => {
+    return prisma.promo.update({
+      where: {
+        id: input.id
+      },
+      data: {
+        apprenants: {
+          connect: {
+            id: input.idU
+          }
+        }
+      }
+    })
+  }),
+
+  removeStudent: protectedProcedure.input(z.object({ id: z.string(), idU: z.string() })).mutation(({ input }) => {
+    return prisma.promo.update({
+      where: {
+        id: input.id
+      },
+      data: {
+        apprenants: {
+          disconnect: {
+            id: input.idU
+          }
+        }
+      }
+    })
+  }),
+
+});
